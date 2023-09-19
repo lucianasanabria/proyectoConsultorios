0c3499047d9c2c826673c1a0d8995955dbe60670
lucianasanabria <luciana.sanabria@hotmail.com>
2023-09-19 20:54:42 -0300

    agregando js

---
+++ js/pruebaAlertSacarTurno.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function() {
+    document.getElementById("verAlerta").addEventListener("click", function(){
+        alert("Usted ha enviado la información necesaria para gestionar el turno, en breve nos pondremos en contacto para confirmar disponibilidad.")
+    })
+
+})
