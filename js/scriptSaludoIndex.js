b95e56bc8894269c5a2e6662bde1b65967cbf4fa
lucianasanabria <luciana.sanabria@hotmail.com>
2023-10-26 06:22:55 -0300

    cambios varios en proceso

--- js/scriptSaludoIndex.js
+++ js/scriptSaludoIndex.js
@@ -1,6 +1,6 @@
-function saludar (){
-    alert ("Bienvenid@ " + nombre + " a Consultorios Ingenias");
-}
-let nombre = prompt ("Hola terrícola! Por favor, ingrese su nombre")
+// function saludar (){
+//     alert ("Bienvenid@ " + nombre + " a Consultorios Ingenias");
+// }
+// let nombre = prompt ("Hola terrícola! Por favor, ingrese su nombre")
 
-saludar(nombre);
+// saludar(nombre);
